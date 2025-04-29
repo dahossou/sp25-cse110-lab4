66d6d940b30a005d88ec36299c3839aaf42ecdf1
dahossou <danielahossou@gmail.com>
2025-04-29 15:28:08 -0700

    fixed minor error

--- expose/javascript/part2-question16.js
+++ expose/javascript/part2-question16.js
@@ -7,7 +7,7 @@ let statistics = {
   rareCars: 2
 };
 
-for(property in statistics){
+for(let property in statistics){
   if(property.startsWith('r') || statistics[property] % 2 == 1){
   console.log(`${property}: ${statistics[property]}`);
         }
